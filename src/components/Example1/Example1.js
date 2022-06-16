ccf13bb5eb8ab77e8067bec9a290ebc7b6ae7427
Pranav <pvenkatesh3141@gmail.com>
2022-06-16 12:51:28 -0500

    Example1

---
+++ src/components/Example1/Example1.js
@@ -0,0 +1,27 @@
+import {useState, useEffect} from 'react'
+
+const Example1 = (props) => {
+    
+    const [count, setCount] = useState(0)
+    const [textColor, setTextColor] = useState("black")
+
+    useEffect(() => {
+        if (props.color === "r") {
+            setTextColor("red")
+        } else if (props.color === "b") {
+            setTextColor("blue")
+        } else if (props.color === "g") {
+            setTextColor("green")
+        }
+
+    }, [props.color])
+
+    return (
+        <div>
+            <h2 style={{color: textColor}}>Current Count: {count}</h2>
+            <button onClick = {() => {setCount(count + 1)}}>Increment Count</button>
+        </div>
+    )
+}
+
+export default Example1
